17154bf58660d319faf5ccde3f4e740ac19d0a7d
Giuseppe Iellamo <giuseppe.iellamo@altorobotics.ai>
2023-02-22 12:01:14 +0100

    Improved toolchain file

--- aarch64.cmake
+++ aarch64.cmake
@@ -6,12 +6,17 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_CROSSCOMPILING TRUE)
 
+if("$ENV{SYSROOT}" STREQUAL "" )
+  message(FATAL_ERROR "SYSROOT environment variables not defined !!!!")
+endif()
+
 # Name of C compiler.
 set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++")
 
 # Where to look for the target environment. (More paths can be added here)
-set(CMAKE_FIND_ROOT_PATH /home/giellamo/roots )
+set(CMAKE_SYSROOT $ENV{SYSROOT})
+set(CMAKE_FIND_ROOT_PATH $ENV{SYSROOT} )
 set(CMAKE_INCLUDE_PATH  /usr/include/aarch64-linux-gnu)
 set(CMAKE_LIBRARY_PATH  /usr/lib/aarch64-linux-gnu)
 set(CMAKE_PROGRAM_PATH  /usr/bin/aarch64-linux-gnu)
@@ -24,12 +29,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-
-# Specify the target file system
-set(CMAKE_SYSROOT /home/giellamo/roots)
 
 # This assumes that pthread will be available on the target system
 # (this emulates that the return of the TRY_RUN is a return code "0")
 set(THREADS_PTHREAD_ARG "0"
   CACHE STRING "Result from TRY_RUN" FORCE)
+  
